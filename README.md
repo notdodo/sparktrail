f58e01559eef873ea26b8864f27f6647a02fcdb5
Edoardo Rosa <6991986+notdodo@users.noreply.github.com>
2023-03-06 10:13:32 +0100

    init

---
+++ README.md
@@ -0,0 +1,23 @@
+# SparkTrail
+
+Use this Python script to start a Spark standalone session to interact with the CloudTrail bucket.
+Spark allow to query the logs using a SQL-like syntax.
+
+The startup `main.py` script will automatically load SSO credentials and set AWS temporary credentials from the SSO to authenticated to the bucket.
+
+## Usage
+
+0. Spawn Poetry shell:
+
+`poetry shell`
+
+1. Start the cluster:
+
+`PYSPARK_DRIVER_PYTHON=ipython PYTHONSTARTUP=main.py pyspark --packages org.apache.hadoop:hadoop-aws:3.3.4,com.amazonaws:aws-java-sdk-bundle:1.12.262 --name SparkTrail`
+
+2. Now the environment is configured and to start running queries link the S3 bucket. When the IPython shell is return link the bucket and start performing queries:
+
+```python
+spark = link_s3("s3a://audit-cloudtrail-logs/AWSLogs/")
+spark.select("Records.eventName").distinct().show(10)
+```
---
+++ main.py
@@ -0,0 +1,77 @@
+import json
+import os
+from configparser import ConfigParser
+from hashlib import sha1
+from pathlib import Path
+
+import boto3
+from pyspark import SparkConf, SparkContext
+from pyspark.sql import SparkSession
+
+
+def read_aws_config():
+    config = ConfigParser()
+    config.read(Path("~/.aws/config").expanduser().absolute())
+    return config["profile " + os.environ["AWS_PROFILE"]]
+
+
+def is_sso():
+    profile = read_aws_config()
+    try:
+        sso_url = [i for i in profile if "sso_start_url" == i][0]
+        return profile[sso_url]
+    except KeyError:
+        return False
+
+
+def get_sso_cached_login(sso_url):
+    cache = sha1(sso_url.encode()).hexdigest()
+    sso_cache_file = Path(f"~/.aws/sso/cache/{cache}.json").absolute()
+    return json.loads(sso_cache_file.absolute().read_text())
+
+
+def get_sso_credentials():
+    if sso_url := is_sso():
+        sso = boto3.session.Session(
+            profile_name=os.environ["AWS_PROFILE"],
+        ).client("sso")
+        profile = read_aws_config()
+        return sso.get_role_credentials(
+            roleName=profile["sso_role_name"],
+            accountId=profile["sso_account_id"],
+            accessToken=get_sso_cached_login(sso_url)["accessToken"],
+        )["roleCredentials"]
+
+
+def configure_spark():
+    credentials = get_sso_credentials()
+    conf = (
+        SparkConf()
+        .set("spark.sql.debug.maxToStringFields", 10000)
+        .set(
+            "spark.hadoop.fs.s3a.impl",
+            "org.apache.hadoop.fs.s3a.S3AFileSystem",
+        )
+        .set("spark.hadoop.com.amazonaws.services.s3.enableV4", "true")
+        .set(
+            "spark.jars.packages",
+            "org.apache.hadoop:hadoop-aws:3.2.2,com.amazonaws:aws-java-sdk-bundle:1.11.888",
+        )
+        .set(
+            "spark.hadoop.fs.s3a.aws.credentials.provider",
+            "org.apache.hadoop.fs.s3a.TemporaryAWSCredentialsProvider",
+        )
+        .set("spark.hadoop.fs.s3a.access.key", credentials["accessKeyId"])
+        .set("spark.hadoop.fs.s3a.secret.key", credentials["secretAccessKey"])
+        .set("spark.hadoop.fs.s3a.session.token", credentials["sessionToken"])
+    )
+    sc = SparkContext.getOrCreate()
+    sc.stop()
+    sc = SparkContext(conf=conf)
+    return SparkSession(sc)
+
+
+def link_s3(bucket):
+    spark = configure_spark()
+    collect = spark.read.format("json").load(f"s3a://{bucket}")
+    return collect

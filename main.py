d9f25af1a57076ef8a1b0c76757830731716f086
Edoardo Rosa <6991986+notdodo@users.noreply.github.com>
2024-01-16 16:30:53 +0100

    enh: autofetch SSO creds

--- main.py
+++ main.py
@@ -9,38 +9,16 @@ from pyspark import SparkConf, SparkContext
 from pyspark.sql import SparkSession
 
 
-def read_aws_config():
-    config = ConfigParser()
-    config.read(Path("~/.aws/config").expanduser())
-    return config["profile " + os.environ["AWS_PROFILE"]]
-
-
-def is_sso():
-    profile = read_aws_config()
-    try:
-        sso_url = [i for i in profile if "sso_start_url" == i][0]
-        return profile[sso_url]
-    except KeyError:
-        return False
-
-
-def get_sso_cached_login(sso_url):
-    cache = sha1(sso_url.encode()).hexdigest()
-    sso_cache_file = Path(f"~/.aws/sso/cache/{cache}.json").expanduser()
-    return json.loads(sso_cache_file.read_text())
-
-
 def get_sso_credentials():
-    if sso_url := is_sso():
-        sso = boto3.session.Session(
-            profile_name=os.environ["AWS_PROFILE"],
-        ).client("sso")
-        profile = read_aws_config()
-        return sso.get_role_credentials(
-            roleName=profile["sso_role_name"],
-            accountId=profile["sso_account_id"],
-            accessToken=get_sso_cached_login(sso_url)["accessToken"],
-        )["roleCredentials"]
+    profile_name = os.environ["AWS_PROFILE"]
+    boto3.setup_default_session(profile_name=profile_name)
+    session = boto3.session.Session()
+    creds = session.get_credentials().get_frozen_credentials()
+    return {
+        "accessKey": creds.access_key,
+        "secretAccessKey": creds.secret_key,
+        "sessionToken": creds.token,
+    }
 
 
 def configure_spark():
@@ -55,13 +33,13 @@ def configure_spark():
         .set("spark.hadoop.com.amazonaws.services.s3.enableV4", "true")
         .set(
             "spark.jars.packages",
-            "org.apache.hadoop:hadoop-aws:3.2.2,com.amazonaws:aws-java-sdk-bundle:1.11.888",
+            "org.apache.hadoop:hadoop-aws:3.3.4,com.amazonaws:aws-java-sdk-bundle:1.12.262",
         )
         .set(
             "spark.hadoop.fs.s3a.aws.credentials.provider",
             "org.apache.hadoop.fs.s3a.TemporaryAWSCredentialsProvider",
         )
-        .set("spark.hadoop.fs.s3a.access.key", credentials["accessKeyId"])
+        .set("spark.hadoop.fs.s3a.access.key", credentials["accessKey"])
         .set("spark.hadoop.fs.s3a.secret.key", credentials["secretAccessKey"])
         .set("spark.hadoop.fs.s3a.session.token", credentials["sessionToken"])
     )
